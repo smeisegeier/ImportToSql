27f2f35c8f3b57d9c96e254e0a52b29081361fb9
j.graef <j.graef@rki.de>
2021-07-21 15:27:39 +0200

    DataTableHelper

---
+++ Helper/CsvHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rki.ImportToSql.Helper
+{
+    static class CsvHelper
+    {
+
+        public static DataTable readCsv(string path, char seperator)
+        {
+            DataTable _table = new DataTable();
+            String[] csv = File.ReadAllLines(@path);
+            string[] zeile = new string[csv.Length];
+
+            int anzahlColumns = 0;
+
+            for (int i = 0; i < csv.Length; i++)
+            {
+                zeile = csv[i].Split(seperator);
+
+                if (i == 0)
+                {
+                    foreach (string item in zeile)
+                    {
+                        if (_table.Columns[item] == null)
+                        {
+                            _table.Columns.Add(item);
+                        }
+                        else
+                        {
+                            bool newNameFound = false;
+                            int j = 1;
+                            while (!newNameFound)
+                            {
+                                if (_table.Columns[item + '_' + j] == null)
+                                {
+                                    _table.Columns.Add(item + "_" + j);
+                                    newNameFound = true;
+                                }
+                                j++;
+                            }
+                        }
+                        anzahlColumns++;
+                    }
+                }
+
+                //Fehler wird nur geworfen, wenn es mehr Inhalte als es Spalten gibt
+                if (zeile.Length > anzahlColumns) throw new Exception("Anzahl der Spalten ist nicht gleichbleibend!");
+                _table.Rows.Add(zeile);
+            }
+
+            return _table;
+        }
+
+    }
+}
---
+++ Helper/DataTableHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rki.ImportToSql.Helper
+{
+    class DataTableHelper
+    {
+        private DataTable dataTable;
+
+        
+
+        public DataTableHelper(DataTable dataTable)
+        {
+            this.dataTable = dataTable;
+        }
+
+    }
+
+    class Coulmn
+    {
+        private String header;
+        private ContentType type;
+        private List<ColumnContent> columnContents;
+
+        public Coulmn(DataColumn dataColumn)
+        {
+            if (dataColumn.Table.Rows.Count > 1)
+            {
+                header = dataColumn.ColumnName;
+                type = ContentType.STRING;
+
+                for(int i = 1; i < dataColumn.Table.Rows.Count; i++)
+                {
+                    columnContents.Add(new ColumnContent(dataColumn.Table.Rows[i].ToString(), ContentType.STRING));
+                }
+
+            }
+        }
+
+        public bool parseIntoContentType(ContentType type)
+        {
+            foreach(ColumnContent item in columnContents)
+            {
+                if (item.tryParseIntoContentType(type) == false) return false;
+            }
+            this.type = type;
+
+            return true;
+        }
+
+
+        public String getColumnHeader()
+        {
+            return header;
+        }
+
+    }
+
+    class ColumnContent
+    {
+        private ContentType type;
+        private String content;
+
+
+        public ColumnContent(String content, ContentType type)
+        {
+            this.content = content;
+            this.type = type;
+        }
+
+        public bool tryParseIntoContentType(ContentType type)
+        {
+            switch (type)
+            {
+                case ContentType.STRING:
+                    type = ContentType.STRING;
+                    break;
+                case ContentType.INT:
+
+                    try
+                    {
+                        int.Parse(content);
+                        type = ContentType.INT;
+                    } catch { return false; };
+                    break;
+
+                case ContentType.DATETIME:
+                    try
+                    {
+                        DateTime.Parse(content);
+                        type = ContentType.DATETIME;
+                    }
+                    catch { return false; };
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
+    enum ContentType {INT, STRING, DATETIME}
+
+
+}
